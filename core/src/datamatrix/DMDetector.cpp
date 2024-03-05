55338bc07153051948bc52585ef6d23d1bd1fa25
pixml27 <pixml333@gmail.com>
2024-03-05 14:20:51 +0300

    Parfenov fix v2

--- core/src/datamatrix/DMDetector.cpp
+++ core/src/datamatrix/DMDetector.cpp
@@ -925,9 +925,10 @@ DetectorResults Detect(const BitMatrix& image, bool tryHarder, bool tryRotate, b
 			co_yield std::move(r);
 		}
 		if (!found && tryHarder) {
-			if (auto r = DetectPure(image); r.isValid())
-				co_yield std::move(r);
-			else if(auto r = DetectOld(image); r.isValid())
+			//if (auto r = DetectPure(image); r.isValid())
+			//	co_yield std::move(r);
+			//else 
+			if(auto r = DetectOld(image); r.isValid())
 				co_yield std::move(r);
 		}
 	}
@@ -936,8 +937,8 @@ DetectorResults Detect(const BitMatrix& image, bool tryHarder, bool tryRotate, b
 		return DetectPure(image);
 
 	auto result = DetectNew(image, tryHarder, tryRotate);
-	if (!result.isValid() && tryHarder)
-		result = DetectPure(image);
+	//if (!result.isValid() && tryHarder)
+	//	result = DetectPure(image);
 	if (!result.isValid() && tryHarder)
 		result = DetectOld(image);
 	return result;
