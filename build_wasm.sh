c23a4912819a87b0925cfeebfaf97d12a4e35853
Chorbier <dssen@ya.ru>
2025-12-26 13:42:11 +0500

    wasm minimal working stuff

---
+++ build_wasm.sh
@@ -0,0 +1,21 @@
+CMAKE_TOOLCHAIN_FILE="/home/chorbier/emsdk/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake"
+
+mkdir -p build_wasm
+
+cd build_wasm
+
+pyenv local 3.13
+
+OPENCV_PATH="/home/chorbier/opencv_wasm/build_js/"
+
+# Build for WASM
+echo "Building for WASM"
+emcmake cmake -B . -S .. -DCMAKE_TOOLCHAIN_FILE="$CMAKE_TOOLCHAIN_FILE" -DOpenCV_DIR="$OPENCV_PATH" -DBUILD_FOR_WASM=ON
+if [ ! -f "Makefile" ]; then
+    echo "❌ CMake failed to generate Makefile for WASM"
+    exit 1
+fi
+
+make -j 8
+
+echo "Build complete! Libraries are in the respective architecture directories."
